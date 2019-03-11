2493d7ddd1897971294550ff33ee75e03a9917ff
Maximilian Tiao <maximilian.tiao@gmail.com>
2019-03-10 22:00:57 -0700

    finisished basic outcome of skin grade

--- caseOpeningSim.py
+++ caseOpeningSim.py
@@ -1,12 +1,13 @@
 # CSGO Danger Zone case opening simulator
 
 import time
+import random
+import decimal
 
 
-    
 
 def walletCheckAdd():
-        
+    
     wallet = 10
     totalMoneySpent = 0
     costOfCase = 2
@@ -16,16 +17,16 @@ def walletCheckAdd():
     
     print ("\nWould you like to add funds to open Danger Zone case?")
     answer2 =  raw_input("Yes or No?\n")
-        
+    
     time.sleep(0.8)
-        
+    
     if answer2 == "No" or answer2 == "no":
         wallet -= 2
         totalMoneySpent += 2
         pass
-        
+    
     else:
-            
+        
         if answer2 == "Yes" or answer2 == "yes":
             print ("\nEnter amount to add:")
             answer3  = int(raw_input("$5 $10 $15 $20 $50 $100\n"))
@@ -42,17 +43,17 @@ def walletCheckAdd():
     print ("\nOpen Danger Zone case for $2?")
     
     answer1 = raw_input("Yes or No?\n")
-    
+
     if answer1 == "Yes" or answer1 == "yes":
         
         wallet -= 2
         totalMoneySpent += 2
         
         time.sleep(1)
-        
+
     else:
         pass
-    
+
     time.sleep(2)
 
     print ("\nYour Receipt:")
@@ -63,36 +64,39 @@ def walletCheckAdd():
 
 
 def skinGradeOdds():
-# probabilities of certain grades: blue, purple, pink, red, yellow
-
+    # probabilities of certain grades: blue, purple, pink, red, yellow
+    
     skinGrade = ""
-             
-    random = uniform(2.62, 100.0) #fix random number generator
-
-    if random >= 20 and random < 100:
-    # drops a blue grade skin
-        skinGrade += blue
-        return skinGrade
-
-    elif random >= 3 and random < 20:
-    # drops a purple grade skin
-        skinGrade += purple
-        return skinGrade
-
-    elif random >= 2.91 and random < 3:
-    # drops a pink grade skin
-        skinGrade += pink
-        return skinGrade
         
-    elif random >= 2.70 and random < 2.91:
-    # drops a red grade skin
-        skinGrade += red
-        return skinGrade
+    skinGradeProb = random.randrange(262, 10100)/100
 
-    elif random >= 2.62 and random < 2.7:
+    if skinGradeProb >= 20.0 and skinGradeProb < 100.0:
+    # drops a blue grade skin
+        skinGrade += "blue"
+        print (skinGrade)
+        
+    elif skinGradeProb >= 3.0 and skinGradeProb < 20.0:
+    # drops a purple grade skin
+        skinGrade += "purple"
+        print (skinGrade)
+        
+    elif skinGradeProb >= 2.91 and skinGradeProb < 3.0:
+    # drops a pink grade skin
+        skinGrade += "pink"
+        print (skinGrade)
+        
+    elif skinGradeProb >= 2.70 and skinGradeProb < 2.91:
+    # drops a red grade skin
+        skinGrade += "red"
+        print (skinGrade)
+        
+    elif skinGradeProb >= 2.62 and skinGradeProb < 2.7:
     # drops a yellow grade skin
-        skinGrade += yellow
-        return skinGrade
+        skinGrade += "yellow"
+        print (skinGrade)
+        
+    else:
+        print ("error")
 
 
     #def skinGradeStatTrak():
@@ -104,7 +108,10 @@ def skinGradeOdds():
 
 
 # function calling
-walletCheckAdd()
+#walletCheckAdd()
+
+skinGradeOdds()
+
 
 
 
