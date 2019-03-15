6c98e4372f93d09b113753d2b5a6fe9f874d235b
Maximilian Tiao <maximilian.tiao@gmail.com>
2019-03-14 21:31:44 -0700

    WIP point system

--- rpsGameRandom.py
+++ rpsGameRandom.py
@@ -1,10 +1,19 @@
 # Rock Paper Scissors Mini Game randomly generated
+
+
 import random
 
 
+# just some variables
+
+player1Score = 0
+player2Score = 0
+    
+player1Choice = ""
+player2Choice = ""
+
 # function to start the game, Rock Paper Scissors
 def startGame():
-
 	
     startGameYorN = raw_input("Would you like to start playing Rock Paper Scissors?\n")
 	
@@ -25,7 +34,8 @@ def startGame():
 		# No to end the game
             exit()
 
-	# randomly generated choices for Player 1 and 2 in the game, Rock Paper Scissors
+
+# randomly generated choices for Player 1 and 2 in the game, Rock Paper Scissors
 def rpsRandom():
 
     player1Score = 0
@@ -40,17 +50,14 @@ def rpsRandom():
     if randomForP1 == 1:
     # Player 1 chooses Rock!
         player1Choice += "Rock!"
-        print(player1Choice)
         
     elif randomForP1 == 2:
     # Player 1 chooses Paper!
         player1Choice += "Paper!"
-        print(player1Choice)
 
     else:
     # Player 1 chooses Scissors!
         player1Choice += "Scissors!"
-        print(player1Choice)
 
     # randomly generate a number associated with a choice to Playeer 2
     randomForP2 = random.randrange(1,4)
@@ -58,20 +65,51 @@ def rpsRandom():
     if randomForP2 == 1:
     # Player 2 chooses Rock!
         player2Choice += "Rock!"
-        print(player2Choice)
 
     elif randomForP2 == 2:
     # Player 2 chooses Paper!
         player2Choice += "Paper!"
-        print(player2Choice)
 
     else:
     # Player 2 chooses Scissors!
         player2Choice += "Scissors!"
-        print(player2Choice)
+
+
+# point system for wins
+def pointSystem(player1Choice, player2Choice):
+
+    if player1Choice == "Rock":
+
+        if player2Choice == "Rock":
+            
+            print("It's a tie!")
+
+        elif player2Choice == "Paper":
+            player2Score += 1
+            print("Player 2 wins!")
+            print("Player 1 score: %1d" % (player1Score))
+            print("Player 2 score: %1d" % (player2Score))
+
+        elif player1Choice == "Scissors":
+            player1Score += 1
+            print("Player 1 wins!")
+            print("Player 1 score: %1d" % (player1Score))
+            print("Player 2 score: %1d" % (player2Score))
+
+        else:
+            pass
+
+
+
+
+
+
+
 
 # function calling
 
 startGame()
 
 rpsRandom()
+
+
