d33377799b56784a7bf7819f21126bf5816bbcfc
Maximilian Tiao <maximilian.tiao@gmail.com>
2019-08-27 12:01:46 -0700

    check_palindrome.py is a program that check if user input of an integer is a palindrome or not

---
+++ check_palindrome.py
@@ -0,0 +1,41 @@
+def check_palindrome(numbers):
+    # Variable Declaration
+    numbers_str = str(numbers)
+    numbers_backwards = ""
+
+    # Check input if contain any letters
+    if (numbers_str.isalpha()):
+        print("There is a letter in the input!")
+    else:
+        pass
+
+    # Iterate backwards through input string
+    count = len(numbers_str) - 1
+    while (count >= 0):
+        numbers_backwards += numbers_str[count]
+        count -= 1
+
+    # Check if input is a palindrome
+    if (numbers_str == numbers_backwards):
+        return "The number is a palindrome!"
+    else:
+        return "The number is not a palindrome!"
+
+
+number_input = raw_input("Enter a number: ")
+print("\n")
+print(check_palindrome(number_input))
+print("\n")
+
+yes_or_no = raw_input("Want to input another number? (Y / N): ")
+print("\n")
+
+while (yes_or_no == 'Y' or yes_or_no == 'y'):
+    number_input = raw_input("Enter a number: ")
+    print("\n")
+    print(check_palindrome(number_input))
+    print("\n")
+
+    yes_or_no = raw_input("Want to input another number? (Y / N): ")
+print("\n")
+
