3c4fc05c9ae522ac14dc7ada99121501e43d6bb4
Maximilian Tiao <maximilian.tiao@gmail.com>
2019-09-03 21:27:33 -0700

    ideas for next program :)

---
+++ basicDatabase.py
@@ -0,0 +1,11 @@
+'''
+Basic Database for Stephen King books
+
+most likely use dictionaries to store books as keys
+and genres, date published, and etc as values
+
+program will allow you to search through date, genres, etc
+also can add books if missing or delete books that are mistakes
+
+
+'''
